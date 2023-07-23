9d2e0a28e77ed878d466d171ddfda060557ad20c
zethson <lukas.heumos@posteo.net>
2023-07-23 23:24:11 +0200

    :sparkles: Add nextflow installation

--- tests/test_base.py
+++
@@ -1,7 +0,0 @@
-from nextflow_lamin_usecases import ExampleClass, example_function
-
-
-def test_dummy():
-    assert example_function("A") == "a"
-    ex = ExampleClass(1)
-    assert ex.bar() == "hello"
