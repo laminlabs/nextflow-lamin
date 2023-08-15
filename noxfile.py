c3586f9c1df4ccc769d04dc31a6a0a111c100829
zethson <lukas.heumos@posteo.net>
2023-08-15 15:51:13 +0200

    :sparkles: Add artifact upload
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,5 @@
 import nox
+from laminci import upload_docs_artifact
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
 # we'd like to aggregate coverage information across sessions
@@ -19,3 +20,4 @@ def build(session):
     login_testuser1(session)
     run_pytest(session)
     build_docs(session)
+    upload_docs_artifact(aws=True)
