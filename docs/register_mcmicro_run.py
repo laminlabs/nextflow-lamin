82dcc5370b381efd493ed33d949dc102d7427271
Lukas Heumos <lukas.heumos@posteo.net>
2024-04-22 08:47:27 -0700

    Simplify (#27)
    * :art: Use script
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :sparkles: Simplify language
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :art: Refactor
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :art: Wording
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :art: Simplify
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    ---------
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>

---
+++ docs/register_mcmicro_run.py
@@ -0,0 +1,25 @@
+from subprocess import getoutput
+
+import lamindb as ln
+
+transform = ln.Transform(
+    name="mcmicro",
+    version="1.0.0",
+    type="pipeline",
+    reference="https://github.com/labsyspharm/mcmicro",
+)
+ln.track(transform=transform)
+run = ln.dev.run_context.run
+
+mcmicro_input = ln.Artifact.filter(key__startswith="exemplar-001")
+input_paths = [input_fastq.stage() for input_fastq in mcmicro_input]
+
+output = ln.Artifact(
+    "exemplar-001/registration/exemplar-001.ome.tif", description="mcmicro"
+)
+output.save()
+
+nextflow_id = getoutput(f"nextflow log | awk '/{run.id}/{{print $8}}'")
+run.reference = nextflow_id
+run.reference_type = "nextflow_id"
+run.save()
