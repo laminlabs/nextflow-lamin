edcbd6b0817a264960f4b5bda8a950a5b8b309ab
Lukas Heumos <lukas.heumos@posteo.net>
2024-06-20 11:24:41 -0700

    ✨ Link reports to run (#37)
    * :sparkles: Add failing linking notebook
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :sparkles: Add new script
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :sparkles: Fix output
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :art: TriggerCI
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * Fix script path
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :art: Pre-commit back
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :art: Remove test
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :art: Add deletion back
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :art: Remove unused code
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    ---------
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- docs/register_mcmicro_run.py
+++ docs/register_mcmicro_run.py
@@ -1,6 +1,7 @@
 from subprocess import getoutput
 
 import lamindb as ln
+import yaml
 
 transform = ln.Transform(
     name="mcmicro",
@@ -8,18 +9,35 @@ transform = ln.Transform(
     type="pipeline",
     reference="https://github.com/labsyspharm/mcmicro",
 )
-ln.track(transform=transform)
-run = ln.dev.run_context.run
+run = ln.track(transform=transform)
+
+ln.settings.sync_git_repo = "https://github.com/laminlabs/nextflow-lamin-usecases"
+
+ulabel = ln.ULabel(name="nextflow").save()
+transform.ulabels.add(ulabel)
+
+with open("exemplar-001/qc/params.yml") as params_file:
+    qc_params = yaml.safe_load(params_file)
+
+ln.Param(name="qc_params", dtype="dict").save()
+run.params.add_values({"qc_params": qc_params})
 
 mcmicro_input = ln.Artifact.filter(key__startswith="exemplar-001")
-input_paths = [input_fastq.cache() for input_fastq in mcmicro_input]
+run.input_artifacts.set(mcmicro_input)
 
 output = ln.Artifact(
     "exemplar-001/registration/exemplar-001.ome.tif", description="mcmicro"
 )
 output.save()
+run.output_artifacts.set([output])
+
+report = ln.Artifact("execution_report.html").save()
+run.report = report
+run.save()
 
 nextflow_id = getoutput(f"nextflow log | awk '/{run.id}/{{print $8}}'")
 run.reference = nextflow_id
 run.reference_type = "nextflow_id"
 run.save()
+
+ln.finish()
