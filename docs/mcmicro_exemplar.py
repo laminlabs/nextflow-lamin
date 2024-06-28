0d532f25814426d12ddb63280d8df8a847c9731e
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-06-28 13:17:44 +0200

    📝 Update

--- docs/mcmicro_exemplar.py
+++ docs/mcmicro_exemplar.py
@@ -15,7 +15,7 @@ args = parser.parse_args()
 if args.name not in ["exemplar-001", "exemplar-002"]:
     raise ValueError("Invalid name. Use 'exemplar-001' or 'exemplar-002'.")
 
-# run the nextflow pipeline to download example data
+# execute the nextflow pipeline to download example data
 report = f"{args.name}-execution_report.html"
 subprocess.run(
     [
@@ -32,7 +32,7 @@ subprocess.run(
 # get the nextflow execution id from the log (first row)
 nextflow_id = subprocess.getoutput("nextflow log | awk 'NR==2{print $7}'")
 
-# create transform
+# track the pipeline transform
 transform = ln.Transform(
     name="MCMICRO exemplar",
     version="1.0.0",
@@ -45,11 +45,14 @@ run.reference = nextflow_id
 run.reference_type = "nextflow_id"
 run.report = report
 run.save()
+# optionally, track the pipeline parameters
 run.params.add_values({"name": args.name})
+# optionally, sync with the git repository
 ln.settings.sync_git_repo = "https://github.com/laminlabs/nextflow-lamin-usecases"
 
 # register mcmicro input data
 mcmicro_input = ln.Artifact.from_dir(args.name)
 ln.save(mcmicro_input)
 
+# optionally, track this python script as source code
 ln.finish()
