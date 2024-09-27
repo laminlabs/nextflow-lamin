0850fa9b0cfacb4f3e1210185dbd0c13f5b3c7da
zethson <lukas.heumos@posteo.net>
2024-09-27 16:21:04 +0200

    :art: Try lineage
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- docs/mcmicro.py
+++ docs/mcmicro.py
@@ -27,9 +27,7 @@ ln.context.track(transform=transform)
 run = ln.context.run
 
 # TEEEEEEEEST
-mcmicro_input = ln.Artifact.get("laminlabs/lamindata").filter(
-    description="exemplar-001"
-)
+mcmicro_input = ln.Artifact.using("laminlabs/lamindata").get(description="exemplar-001")
 mcmicro_input.load()
 
 report = "mcmicro-execution_report.html"
